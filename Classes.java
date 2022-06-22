aa47a8d486650fe0bd389c211cb9050887918546
Mateus Reginaldo <91707367+MtReginaldo@users.noreply.github.com>
2022-06-22 20:15:00 -0300

    Update Classes.java

--- Classes.java
+++ Classes.java
@@ -1,1 +1,15 @@
+public class Caixa{ 
+    
+    public int caixa;
+    public Caixa(String d) { caixa = c; }
+    
+    public String getDado() { return; }
 
+    public static void main(int[] args) {
+    Caixa<Integer> integerBox = new Caixa<int>();
+    Caixa<String> stringBox = new Caixa<int>();
+
+    System.out.printf(caixa.get());
+    System.out.printf(Caixa.get());
+      }
+    }
